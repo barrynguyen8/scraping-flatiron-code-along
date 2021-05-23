b415ffd66ccd97963f2288f324811b69a1b3f3e0
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-05-23 11:36:07 +0000

    assignment submitted

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,19 @@
+class Course
+    attr_accessor :title, :schedule, :description #define getter and setter course instance methods
+    
+    @@all = [] # class variable is an empty array 
+    
+    def initialize #constructor - when ruby creates a new object, it executes this 'constructor' instance method to put default values into instance variables 
+        @@all << self #when a class of this instance is initialized, it will be pushed to the @all class variable 
+    end
 
+    def self.all #class method - returns an array of all the instances of the Course class
+        @@all
+    end
+
+    def self.reset_all #class method
+        @@all.clear
+    end
+    
+end 
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,37 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-require_relative './course.rb'
+require_relative './course.rb' #enables Scraper to make new courses and give them attributes scraped from the web page.
 
 class Scraper
   
-  def print_courses
+  def get_page # instance method that uses Nokogiri and open-uri to get the HTML from a web page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+    # doc.css(".post").each do |post|
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    # end
+  end 
+  
+  def get_courses # uses a CSS selector to return an array of Nokogiri XML elements representing the courses described on the web page we are scraping
+    self.get_page.css(".post")
+  end 
+  
+  def make_courses # iterates over the courses array returned by method get_courses and creates a new Course instance (instantiating) out of each array element
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+  
+  
+  def print_courses # calls on .make_courses and then iterates over all of the courses that get created to puts out a list of course offering
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
@@ -15,8 +41,9 @@ class Scraper
       end
     end
   end
-  
 end
 
+Scraper.new.get_page
+Scraper.new.print_courses
 
 
